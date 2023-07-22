acd6edf24b406a95a55a336941adc219e002adbb
Enotherms <sayabiyyu@gmail.com>
2023-07-22 22:06:51 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/ParticlesBackground.jsx 	deleted:    src/components/config/particles-config.js

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,23 +1,70 @@
 import Particles from "react-tsparticles";
-import particlesConfig from "./config/particles-config";
-import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
 
 function ParticlesBackground() {
-	const particlesInit = async (main) => {
-		console.log(main);
-		await loadFull(main);
-	};
-	const particlesLoaded = (container) => {
-		console.log(container);
-	};
+	const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
 	return (
 		<div className="z-0">
 			<Particles
-				params={particlesConfig}
-				id="tsparticles"
-				init={particlesInit}
-				loaded={particlesLoaded}
-			/>
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                particles: {
+                    color: {
+                        value: "#7289da",
+                    },
+                    links: {
+                        color: "#7289da",
+                        distance: 150,
+                        enable: true,
+                        opacity: 1,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 4 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
 		</div>
 	);
 }
